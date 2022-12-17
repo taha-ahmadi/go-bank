3026c417193184e44331610d3b32a0c80915a578
Taha Ahmadi <tahaahmadimail@protonmail.com>
2022-12-17 18:28:31 +0330

    implement transfer money API with a custom params validator

---
+++ util/currency.go
@@ -0,0 +1,15 @@
+package util
+
+const (
+	USD = "USD"
+	EUR = "EUR"
+	CAD = "CAD"
+)
+
+func IsSupportedCurrency(currency string) bool {
+	switch currency {
+	case USD, CAD, EUR:
+		return true
+	}
+	return false
+}
